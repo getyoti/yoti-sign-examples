02673b7592c0ba0cc5738977e9ce39e02375c60c
echarrod <echarrod@users.noreply.github.com>
2020-05-29 11:29:09 +0100

    SDK-1569: Add .NET demo

---
+++ dotnet/README.md
@@ -0,0 +1,15 @@
+# .NET Yoti Sign Example
+
+## Setup
+
+1) Register for Yoti Sign at <https://www.yotisign.com/app/contact-us/>
+1) Request an API key (Bearer Authentication Token)
+
+## Running the example
+
+1) Clone this repo
+1) Run `cd dotnet/YotiSignDemo`
+1) Rename `.env.example` to `.env`
+1) Add your Bearer Authentication Token to the `YOTI_AUTHENTICATION_TOKEN` variable in the `.env` file
+1) Run `dotnet run`
+1) visit <https://localhost:5001/sign>
---
+++ dotnet/YotiSignDemo/GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Style", "IDE0063:Use simple 'using' statement", Justification = "Preview feature, not available in all C# versions", Scope = "member", Target = "~M:YotiSignDemo.Controllers.SignController.Get~Microsoft.AspNetCore.Mvc.IActionResult")]
+[assembly: SuppressMessage("Design", "CA1052:Static holder types should be Static or NotInheritable", Justification = "Auto-generated", Scope = "type", Target = "~T:YotiSignDemo.Program")]
